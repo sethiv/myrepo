8c871e4c8ead491dfb3d2fb261e521fe9a0d9a64
sethiv <sethiv>
2021-06-15 16:47:31 +0000

    value in widget

--- scratch.py
+++ scratch.py
@@ -3,14 +3,7 @@ pip install nbdev
 
 # COMMAND ----------
 
-dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
-
-dbutils.widgets.dropdown("1", "1", [str(x) for x in range(1, 10)], "hello this is a widget")
-
-dbutils.widgets.dropdown("x123123", "1", [str(x) for x in range(1, 10)], "hello this is a widget")
-
-dbutils.widgets.dropdown("x1232133123", "1", [str(x) for x in range(1, 10)], "hello this is a widget 2")
-
+dbutils.widgets.dropdown("a dummy widget", "1", [str(x) for x in range(1, 10)])
 
 # COMMAND ----------
 
