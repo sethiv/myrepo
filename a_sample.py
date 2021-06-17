0d14b21a9c47c7abaf292802296fdf52d18d25c7
sethiv <sethiv>
2021-06-17 17:19:08 +0000

    a comment added

--- a_sample.py
+++ a_sample.py
@@ -7,4 +7,9 @@ dbutils.widgets.dropdown("a dropdown widget", "1", [str(x) for x in range(1, 10)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC a change which is not there in other notebook
+
+# COMMAND ----------
+
 
